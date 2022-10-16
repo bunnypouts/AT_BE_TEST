090f8c2cb95302e6d962f4d8a6ff81988994e19a
Shaikh <Asif.Shaikh@gilbarco.com>
2022-10-16 16:29:19 +0530

    models generated

---
+++ app/Models/EmployeesContacts.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeesContacts extends Model
+{
+    use HasFactory;
+}
